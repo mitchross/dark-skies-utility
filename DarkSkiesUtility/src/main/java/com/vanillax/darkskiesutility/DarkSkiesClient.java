f54bdde3e1a3541bd260ecf543a6ab8279b88f8a
mitchross09 <mitchross09@gmail.com>
2013-12-14 14:13:16 -0500

    First commit

---
+++ DarkSkiesUtility/src/main/java/com/vanillax/darkskiesutility/DarkSkiesClient.java
@@ -0,0 +1,93 @@
+package com.vanillax.darkskiesutility;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import retrofit.RestAdapter;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
+/**
+ * Created by mitchross on 12/11/13.
+ */
+public class DarkSkiesClient
+{
+
+	public static final String Dark_URL = "https://api.forecast.io/forecast/6ed261da1e77a14fd6e183cec77f642e";
+
+
+	public static class WeatherInfo{
+
+		String timezone;
+		Currently currently;
+		Hourly hourly;
+	}
+
+	public static class Currently {
+		String summary;
+		double nearestStormDistance;
+		double nearestStormBearing;
+		double precipIntensity;
+		double precipProbability;
+		double temperature;
+		double dewPoint;
+		double humidity;
+		double windSpeed;
+		double windBearing;
+		double visibility;
+		double cloudCover;
+		double pressure;
+		double ozone;
+
+
+	}
+
+	public static class Hourly {
+		String summary;
+		//This "data' has to match JSON key
+		List<DataForHourly> data;
+	}
+
+	public static class DataForHourly{
+		int time;
+		String summary;
+	}
+
+	public interface Forecast{
+		@GET("/{latlong}")
+		WeatherInfo  weatherLocation(
+				@Path( "latlong" ) String latlong );
+	}
+
+
+	public static void main(String... args) {
+
+		RestAdapter restAdapter = new RestAdapter.Builder().setServer( Dark_URL ).build();
+
+		Forecast forecast = restAdapter.create( Forecast.class );
+
+		WeatherInfo w = forecast.weatherLocation( "42,-85" );
+
+		System.out.println( w.timezone + " break " + w.currently.summary );
+		System.out.println( w.timezone + " break " + w.currently.cloudCover );
+
+		List<DataForHourly> hours = w.hourly.data;
+		for(DataForHourly d : hours)
+		{
+
+			long dv = Long.valueOf(d.time)*1000;// its need to be in milisecond
+			Date df = new java.util.Date(dv);
+			String vv = new SimpleDateFormat("MM dd, yyyy hh:mma").format(df);
+
+			System.out.println("hour " + vv + " summary " + d.summary);
+		}
+
+		System.out.println(" size "  + w.hourly.data.size());
+
+
+
+
+
+	}
+}
